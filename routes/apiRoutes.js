3f2bb832c6cd87ed595edbfa4d33d1b96b4646ec
Emmanuel Durand <shernua@gmail.com>
2020-10-16 13:46:10 -0500

    working on apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,24 @@
-const router = require("express").Router();
-const store = require("../db/store");
+const fs = require("fs");
+const notes = require("../db/db");
 
-// GET "/api/notes" responds with all notes from the database
-router.get("/notes", (req, res) => {
-  store
-    .getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
-});
+module.exports = function(app) {
 
-router.post("/notes", (req, res) => {
-  store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
-});
+    // grabs info from the db.json file
+    app.get("/api/notes", function(req, res) {
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/notes/:id", (req, res) => {
-  store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
-});
 
-module.exports = router;
+    // adds info to db.json file
+    app.post("/api/notes", function(req, res) {
+        tableData.push(req.body);
+        res.json(notes);
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+
+    });
+
+
+}
